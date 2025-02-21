7ebc32216f4c630057b6574ed626b6fa06549d6c
ukaszoro <ukasz123456@gmail.com>
2025-02-21 13:27:47 +0100

    WebApi: Created Basic Session system
    - Created SessionController class with endpoints 'api/Session/login' and '/api/Session/check' for creating and checking cookies

---
+++ src/WebApi/Controllers/SessionController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using WebApi.SessionManager;
+
+namespace WebApi.Controllers;
+
+[Route("api/Session")]
+[ApiController]
+public class SessionController(ISessionManager sessionManager) : ControllerBase
+{
+    [HttpPost("login")]
+    public ActionResult Login()
+    {
+        Response.Cookies.Append("session", sessionManager.NewSession(),new CookieOptions { Expires = DateTimeOffset.UtcNow.AddDays(30) });
+        return Ok();
+    }
+
+    [HttpPost("check")]
+    public ActionResult Check()
+    {
+        if (sessionManager.IsLoggedIn(Request.Cookies["session"] ?? String.Empty))
+        {
+            return Ok("Logged in");
+        }
+        else
+        {
+            return Unauthorized();
+        }
+    }
+}
