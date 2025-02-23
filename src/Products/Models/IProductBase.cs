1a3f7f20ee9c70dadb6a93713b89f23a74ca9a9a
ukaszoro <ukasz123456@gmail.com>
2025-02-23 17:42:32 +0100

    Product: Removed IProductBase and ProductDto
    - Removed unused Product.secret originally used for testing
    - Removed unneeded ProductDTO and IProductBase

--- src/Products/Models/IProductBase.cs
+++
@@ -1,8 +0,0 @@
-namespace Products.Models;
-
-public interface IProductBase
-{
-    public long Id { get; set; }
-    public string Name { get; set; }
-    public double Price { get; set; }
-}
--- src/Products/Models/Product.cs
+++ src/Products/Models/Product.cs
@@ -1,9 +1,8 @@
 namespace Products.Models;
 
-public class Product : IProductBase
+public class Product
 {
     public long Id { get; set; }
     public string Name { get; set; }
     public double Price { get; set; }
-    public string? Secret { get; set; }
 }
